02eeafe753549024a894112107001b03da906256
Valentina <valentinarasuli@gmail.com>
2023-05-27 12:31:21 +0300

    update project

--- create_order_track_test.py
+++ create_order_track_test.py
@@ -1,16 +1,12 @@
 import data
 import sender_state_request
 
+
 # Функция для проверки, что код ответа равен 200.
-def get_order_test():
-    track = sender_state_request.get_new_order_track()
+def get_order_test(body):
+    track = sender_state_request.get_new_order_track(body)
     res = sender_state_request.get_order(track)
-    print(res.status_code)
     assert res.status_code == 200
 
-get_order_test()
 
-
-
-
-
+get_order_test(data.order)
--- sender_state_request.py
+++ sender_state_request.py
@@ -4,26 +4,23 @@ import data
 
 
 # Эта функция выполняет запрос на создание заказа
-def post_new_order():
+def post_new_order(body):
     return requests.post(
         url=configuration.URL_SERVICE + configuration.CREAT_NEW_ORDER,
-        json=data.order,
+        json=body,
         headers=data.headers,
     )
 
+
 # Эта функция сохраняет номер трекер заказа
-def get_new_order_track():
-    res = post_new_order()
+def get_new_order_track(body):
+    res = post_new_order(body)
     return res.json()["track"]
 
 
 # Эта функция выполняет запрос на получения заказа по треку заказа
 def get_order(track):
-    r = requests.get(
+    return requests.get(
         url=configuration.URL_SERVICE + configuration.GET_ORDER + str(track),
         headers={"Content-Type": "application/json"}
     )
-
-    return r
-
-
